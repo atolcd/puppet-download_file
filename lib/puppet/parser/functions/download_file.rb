b10a0d6bd0749aeb5845036baf65157bcb9d6e0e
Thomas Broyer <t.broyer@ltgt.net>
2012-02-27 04:02:25 +0100

    Optionally verify the download with a SHA-1 checksum.

--- lib/puppet/parser/functions/download_file.rb
+++ lib/puppet/parser/functions/download_file.rb
@@ -6,7 +6,10 @@ Puppet::Parser::Functions::newfunction(:download_file, :type => :rvalue, :doc =>
        file { '/tmp/test':
          ensure => file,
          source => download_file('files', 'foo/bar/test', 'http://server/test')
-       }") do |args|
+       }
+
+   You can pass a SHA-1 checksum as fourth argument to verify the download.") do |args|
+    require 'digest/sha1'
     require 'fileutils'
     require 'open-uri'
     require 'puppet/file_serving/configuration'
@@ -31,17 +34,21 @@ Puppet::Parser::Functions::newfunction(:download_file, :type => :rvalue, :doc =>
     unless ::FileTest.exist?(file_name) then
       parent_dir = ::File.dirname(file_name)
       ::FileUtils.mkdir_p(parent_dir) unless ::FileTest.exist?(parent_dir)
+      hash = ::Digest::SHA1::new
       ::File.open(file_name, 'wb') do |file|
         open(args[2], 'rb') do |stream|
           until stream.eof?
             buf = stream.readpartial(1024)
             file.write(buf)
+            hash.update(buf)
           end
         end
+      end
+      if args[3] then
+        fail 'Non-matching SHA-1 checksum' unless args[3] == hash.hexdigest
       end
     end
 
     # Don't use #{path} as we changed the value in the 'modules' case
     "puppet:///#{mount}/#{args[1]}"
 end
-
